d860dcb15adb62d0da85edb5bbf765ad8b7a4efe
Muhammad Nasrullah <muhammad.nasrullah@aquila360.com>
2025-03-12 21:28:14 +0500

    add proxy support

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 from fastapi import FastAPI, HTTPException
 from base_models import VideoURLRequest
-from video_processing import get_transcript, summarize_video        
-      
-app = FastAPI()   
+from video_processing import get_transcript, summarize_video
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/ping")
+async def ping():
+    return {"message": "pong"}
+
 
 @app.post("/summary")
 async def get_summary(request: VideoURLRequest):
@@ -10,7 +25,7 @@ async def get_summary(request: VideoURLRequest):
     transcript = get_transcript(video_url)
     if "Error" in transcript:
         raise HTTPException(status_code=400, detail=transcript)
-    
+
     summary = summarize_video(transcript)
     if summary:
         return {"summary": summary}
